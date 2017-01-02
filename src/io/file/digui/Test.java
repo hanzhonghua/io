23a451cbed9edda43fc3102a64c0d11a8c7b7dc7
garry <hzh199094@163.com>
2017-01-02 23:09:24 +0800

    FileNameFilter

--- src/io/file/digui/Test.java
+++ src/io/file/digui/Test.java
@@ -1,7 +1,7 @@
 package io.file.digui;
 
 import java.io.File;
-import java.io.FileFilter;
+import java.io.FilenameFilter;
 
 //测试递归，递归必须有一个出口，不然会导致内存溢出
 public class Test {
@@ -11,7 +11,9 @@ public class Test {
 		//阶乘
 		//System.out.println(getJiecheng(5));
 		//输出指定目录所有已.xx结尾文件
-		getFiles(new File("e://"));
+		//getFiles(new File("e://"));
+		//FileNameFilter
+		getFiles2(new File("d://"));
 		long end = System.currentTimeMillis();
 		System.out.println("程序耗时："+(end-start)+"毫秒");
 	}
@@ -43,6 +45,24 @@ public class Test {
 					System.out.println(file);
 				}
 			}
+		}
+	}
+	
+	//使用FileNameFilter
+	public static void getFiles2(File f){
+		
+		File[] files = f.listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				//System.out.println(name);
+				File file = new File(dir,name);
+				boolean flag = file.isFile();
+				boolean flag2 = name.endsWith(".txt");
+				return flag&&flag2;
+			}
+		});
+		for (File file : files) {
+			System.out.println(file);
 		}
 	}
 }
